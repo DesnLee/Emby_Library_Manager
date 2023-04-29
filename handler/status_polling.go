921e24e9de9bfa4899142881bc3206c618bb5219
DesnLee <jiakun.ui@gmail.com>
2023-04-29 15:29:20 +0800

    refactor: refactor polling status

--- handler/status_polling.go
+++ handler/status_polling.go
@@ -18,35 +18,49 @@ func (i ChatID) Recipient() string {
 func timerStart(ids []string) {
 	for _, id := range ids {
 		// 创建一个定时器每 30 秒调用接口轮询，检查接口返回内容，如果已完成则停止轮询，销毁定时器，如果未完成则继续轮询
-		ticker := time.NewTicker(30 * time.Second)
-		go func(t *time.Ticker, i string) {
-			for {
-				select {
-				case <-t.C:
-					itemList, err := getLibraryItems()
-					if err != "" {
-						fmt.Println("Error: 发送轮询请求失败！")
-					} else {
-						for _, v := range itemList {
-							if v.ItemId == i {
-								if v.RefreshStatus == "Idle" {
-									fmt.Println(i, " 刷新完成，停止轮询...")
-									t.Stop()
-									text := fmt.Sprintf("%s 刷新完成！", v.Name)
-									recipient := ChatID(NotifyChatId)
-									if _, err := Bot.B.Send(recipient, text, tele.ModeMarkdown); err != nil {
-										return
-									}
-								} else if v.RefreshStatus == "Active" {
-									fmt.Println(i, "正在刷新")
-								} else if v.RefreshStatus == "Queued" {
-									fmt.Println(i, "排队中")
-								}
-							}
-						}
-					}
+		go polling(id)
+	}
+}
+
+func polling(i string) {
+	t := time.NewTicker(3 * time.Second)
+	for {
+		<-t.C
+		fmt.Println(i, " 查询一次")
+		itemList, err := getLibraryItems()
+		if err != "" {
+			fmt.Println("Error: 发送轮询请求失败！")
+		} else {
+			ok, text := checkIsDone(i, itemList)
+			if ok {
+				t.Stop()
+				recipient := ChatID(NotifyChatId)
+				if _, err := Bot.B.Send(recipient, text, tele.ModeMarkdown); err != nil {
+					return
 				}
 			}
-		}(ticker, id)
+		}
 	}
+}
+
+func checkIsDone(i string, itemList []EmbyLibrary) (bool, string) {
+	for _, v := range itemList {
+		if v.ItemId != i {
+			continue
+		}
+
+		fmt.Println(v)
+		if v.RefreshStatus == "Idle" {
+			fmt.Println(i, " 刷新完成，停止轮询...")
+			return true, fmt.Sprintf("%s 刷新完成！", v.Name)
+		} else {
+			if v.RefreshStatus == "Active" {
+				fmt.Println(i, "正在刷新")
+			} else if v.RefreshStatus == "Queued" {
+				fmt.Println(i, "排队中")
+			}
+			return false, ""
+		}
+	}
+	return true, "列表异常，未找到项目状态"
 }
